a0fa19d6cd48efd80e05d6155fc4ba369f4e935b
Silverasdf <yhg461@vols.utk.edu>
2024-07-07 22:47:00 -0400

    small change because db changed

--- frontend/src/components/requestList/request.jsx
+++ frontend/src/components/requestList/request.jsx
@@ -3,8 +3,8 @@ import "./request.css";
 export default function Request({ request }) {
   return (
     <div className="request-wrapper">
-      <p className="name">{request.userName}</p>
-      <p className="date">{request.postDate.slice(0, 10)}</p>
+      <p className="name">{request.UserName}</p>
+      <p className="date">{request.PostDate.slice(0, 10)}</p>
     </div>
   );
 }
